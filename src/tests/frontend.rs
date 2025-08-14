64153b9ece1d7bf8cef4429d3509b97943480092
rsunico <rsunico@roblox.com>
2025-08-13 18:52:58 -0700

    removed duped test

--- src/tests/frontend.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn deserialize_treat_indexing_as_noopt_flag() {
-    let config: darklua_core::Configuration =
-        json5::from_str("{ treat_indexing_as_noopt: true }").unwrap();
-    assert!(config.treat_indexing_as_noopt);
-    // round-trip
-    let serialized = json5::to_string(&config).unwrap();
-    assert!(serialized.contains("treat_indexing_as_noopt"));
-} 
